6cf7de83b1d075dde7ca57e81d4d4c73ad733862
Fatimoh Bakare <fatimohbakare3@gmail.com>
2024-07-09 16:10:01 +0100

    changed the input width

--- src/components/Delivery/delModal.jsx
+++ src/components/Delivery/delModal.jsx
@@ -16,7 +16,7 @@ export default function PaymentModal(){
     const { isOpen, onOpen, onClose } = useDisclosure()
     return(
         <div className="payment-modal">
-<Button onClick={onOpen}>Add</Button>
+<Button onClick={onOpen}></Button>
 
 <Modal blockScrollOnMount={true} isOpen={isOpen} onClose={onClose} isCentered>
   <ModalOverlay />
